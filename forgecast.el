3b6441e18ac7d53c4624fa819daaebd6987f8788
grtcdr <ba.tahaaziz@gmail.com>
2022-11-01 20:37:53 +0100

    Fix wrong package name in copyright notice
    I copied the copyright notice from one of my unreleased packages and
    forgot to rename the package in one of the locations.

--- forgecast.el
+++ forgecast.el
@@ -16,7 +16,7 @@
 ;; General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with Stack. If not, see <https://www.gnu.org/licenses/>.
+;; along with Forgecast. If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
